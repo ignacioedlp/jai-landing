9d070490d3d36e5381aee6e04ed78c5c50f46ec2
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2026-04-15 15:22:13 -0300

    feat: Disable prefixing of default locale in i18n routing configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["en", "es"],
     routing: {
-      prefixDefaultLocale: true,
+      prefixDefaultLocale: false,
     },
   },
 });
